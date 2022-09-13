4be543423255caf56a55266c123177bd831b8096
Punkmic <mattheus_ribeiro@outlook.com>
2022-09-13 20:10:38 -0300

    added web scraping

--- webscraping.py
+++ webscraping.py
@@ -1,27 +1,38 @@
 from bs4 import BeautifulSoup as bs
-import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 from time import sleep
+import csv
+import os
 
-business_name = 'ifood'
+business_name = 'amazon'
 base_url = 'https://www.reclameaqui.com.br'
 url_site = f'{base_url}/empresa/{business_name}/lista-reclamacoes/'
-wd = webdriver.Firefox()
+wd = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 wd.get(url_site)
 html = wd.page_source
 page = bs(html,'html.parser')
 boxes = page.find_all('div', {'class':'sc-1pe7b5t-0 bJdtis'})
 
-href_links = [box.find('a').get('href') for box in boxes]
-page_links = [f'{base_url}{link}' for link in href_links]
+href_links = [box.find('a').get('href') for box in boxes][:10]
+page_links = [f'{base_url}{link}' for link in href_links][:10]
 
-data = []
+column_names = ['Title', 'Body']
+rows = []
 for link in page_links:
     wd.get(link)
     sleep(2)
     bs_page = bs(wd.page_source, 'html.parser')
     title = bs_page.find('h1', {'data-testid': 'complaint-title'}).text
     body = bs_page.find('p', {'data-testid':'complaint-description'}).text
-    data = (title, body)
-print(data[1:10])
+    rows.append([title, body])
+
+with open('results/web_scraping_results.csv', 'w') as _f:
+    writer = csv.writer(_f)
+    # write the column names
+    writer.writerow(column_names)
+    #write the rows
+    writer.writerows(rows)
+
 wd.quit()
