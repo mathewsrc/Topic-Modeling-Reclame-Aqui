4915c2698e0664e4ed7dba71bfdeb40e9f5b8581
Punkmic <mattheus_ribeiro@outlook.com>
2022-09-13 21:21:11 -0300

    added headless

--- webscraping.py
+++ webscraping.py
@@ -1,21 +1,23 @@
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.service import Service as FirefoxService
+#from webdriver_manager.firefox import GeckoDriverManager
+#from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from time import sleep
 import csv
 import os
 
-options = Options()
-options.add_argument('--window-size=1920,1080')
-options.add_argument("--headless")
-options.add_argument("--start-maximized")
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+# open it, go to a website, and get results
 
 business_name = 'amazon'
 base_url = 'https://www.reclameaqui.com.br'
 url_site = f'{base_url}/empresa/{business_name}/lista-reclamacoes/'
-wd = webdriver.Firefox(service=FirefoxService(GeckoDriverManager(path = r".\\Drivers").install()), options=options, service_log_path=os.devnull)
+wd = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
 wd.get(url_site)
 html = wd.page_source
 page = bs(html,'html.parser')
@@ -32,6 +34,7 @@ for link in page_links:
     bs_page = bs(wd.page_source, 'html.parser')
     title = bs_page.find('h1', {'data-testid': 'complaint-title'}).text
     body = bs_page.find('p', {'data-testid':'complaint-description'}).text
+    print([title,body])
     rows.append([title, body])
 
 with open('results/web_scraping_results.csv', 'w') as _f:
