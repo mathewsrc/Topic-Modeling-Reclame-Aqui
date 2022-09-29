a26ee97e8a66a2f27e04b553667c88511d7e7ed4
Punkmic <mattheus_ribeiro@outlook.com>
2022-09-29 00:39:53 -0300

    Added notifications

--- webscraping.py
+++ webscraping.py
@@ -4,29 +4,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.firefox.options import Options
+
 from time import sleep
 import csv
 from plyer import notification
 from tqdm import tqdm
-from selenium import TimeoutException
+
 
 def execute(input, output, n, alert):
     base_url = 'https://www.reclameaqui.com.br'
-    wd = get_web_driver()
     writer = get_output_writer(output)
     lines = [line.rstrip() for line in input] 
     for line in tqdm(lines):
-        try:
-            print()
-            print(f'Searching for {line}...............................................')
-            print()
-            data = scrap(line, n, base_url, wd)
-            save_to_output(writer, data)
-        except TimeoutException as ex:
-            continue
+        print()
+        print(f'Searching for {line}...............................................')
+        print()
+        data = scrap(line, n, base_url)
+        save_to_output(writer, data)
     if alert == 'y' or alert == True:
-        showNotification()
-    wd.quit()
+        showNotification(output)
 
 def save_to_output(writer, data):
     for i in data:
@@ -34,7 +34,10 @@ def save_to_output(writer, data):
         writer.writerow(i)
 
 def get_web_driver():
-    wd = webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install()))
+    options = webdriver.FirefoxOptions()
+    # wait until the initial HTML document has been completely loaded and parsed
+    options.page_load_strategy = 'eager'
+    wd = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
     return wd
 
 def get_output_writer(output):
@@ -45,31 +48,54 @@ def get_output_writer(output):
     writer.writerow(column_names)
     return writer
     
-def scrap(name, n, base_url, wd):
-    for page in tqdm(range(1, n)):
-        print(f'Page {page} of {n}')
+def scrap(name, n, base_url):
+    notification.notify(title='Web Scraping',message=f'Collecting data from {name}',timeout=10)
+    content = []
+    wd = get_web_driver()
+    for page in range(2, (2+n)):
+        print(f'\nPage {page} of {(2 + n)}')
+        print()
         url_site = f'{base_url}/empresa/{name.lower()}/lista-reclamacoes/?pagina={page}'
         wd.get(url_site)
-        html = wd.page_source
-        bs_obj = bs(html,'html.parser')
+        sleep(3)
+        source = wd.page_source
+        bs_obj = bs(source,'html.parser')
         boxes = bs_obj.find_all('div', {'class':'sc-1pe7b5t-0 bJdtis'})
 
         href_links = [box.find('a').get('href') for box in boxes]
         page_links = [f'{base_url}{link}' for link in href_links]
+        
+        print('----------------------------------------------------------\n')
+        print('Content \n')
 
         for link in page_links:
-            wd.get(link)
-            sleep(4)
-            bs_page = bs(wd.page_source, 'html.parser')
-            title = bs_page.find('h1', {'data-testid': 'complaint-title'}).text
-            body = bs_page.find('p', {'data-testid':'complaint-description'}).text
-            print([title,body])
-            yield [title, body]
+            try:
+                print(f'Link: {link}')
+                wd.get(link)
+                sleep(2)
+                bs_page = bs(wd.page_source, 'html.parser')
+                title = bs_page.find('h1', {'data-testid': 'complaint-title'}).text
+                body = bs_page.find('p', {'data-testid':'complaint-description'}).text
+                print(f'Title: {title} \nBody: {body[:80]}')
+                print('-----------------------------------------------------------------------------\n')
+                yield [title, body]
+                #content.append([title, body])
+            except WebDriverException as wd:
+                continue
+            except Exception as e:
+                notification.notify(
+                    title='Web Scraping',
+                    message=f'Exception: {e}',
+                    timeout=10
+                )
+                print(e)
+    wd.quit()
+    return content
         
 def showNotification(output):
     notification.notify(
-        title='Web Scraping Finished',
-        message=f'Data collected and save to {output}',
+        title='Web Scraping',
+        message=f'Data collected and saved to {output}',
         timeout=10
     )
     
