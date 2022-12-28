a3eb3342d999bce8aee0fc3eb35ff4938203f767
email <username>
2022-12-28 23:54:27 +0000

    message

--- load_corpus.py
+++ load_corpus.py
@@ -1,11 +1,4 @@
 import pickle
-import os
-
-def load_corpus(option='preprocessed'):
-  prep_file_path = '/content/Topic-Modeling-Reclame-Aqui/corpus/preprocessed'
-  raw_file_path = '/content/Topic-Modeling-Reclame-Aqui/corpus/raw'
-
-  if option == 'raw':
-    return pickle.load(open('%s/corpus.p' %raw_file_path, 'rb'))
-  elif option == 'preprocessed':
-    return pickle.load(open('%s/corpus.p' %prep_file_path, 'rb'))
+def load_corpus(file_name = "corpus.pkl"):
+  file_path = f'./corpus/{file_name}'
+  return pickle.load(open(file_path, 'rb'))
--- save_corpus.py
+++ save_corpus.py
@@ -1,29 +1,11 @@
 import pickle
 import os
-import click
 
-def save_corpus(corpus, option):
-  prep_file_path = '/content/Topic-Modeling-Reclame-Aqui/corpus/preprocessed'
-  raw_file_path = '/content/Topic-Modeling-Reclame-Aqui/corpus/raw'
+def save_corpus(corpus, file_name="corpus.pkl"):
+  file_path = f'./corpus/{file_name}'
 
-  if option == 'raw':
-    if not os.path.exists(raw_file_path):
-      os.makedirs(raw_file_path)
-    pickle.dump(corpus, open('%s/corpus.p' %raw_file_path, 'wb'))
-    print(f"Corpus saved to {raw_file_path}")
-  elif option == 'preprocessed':
-    if not os.path.exists(prep_file_path):
-      os.makedirs(prep_file_path)
-    pickle.dump(corpus, open('%s/corpus.p' %prep_file_path, 'wb'))
-    print(f"Corpus saved to {prep_file_path}")
-
-
-@click.command()
-@click.option('--corpus', required=True)
-@click.option('--option', type=click.Choice(['raw', 'preprocessed']), required=True, prompt="Select one option:")
-def cli(corpus, option):
-  save_corpus(corpus, option)
-
-
-if __name__ =='__main__':
-    cli()
+  if not os.path.exists('./corpus'):
+    os.makedirs('./corpus')
+  with open(file_path, 'wb') as f:
+    # Save the DataFrame to the file
+    pickle.dump(corpus, f)
